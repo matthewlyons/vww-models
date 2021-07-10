abbedbd4b43472e5a6097108a8bd06703c13ecf2
Matthew Lyons <matthewwaynelyons@live.com>
2021-07-10 00:05:56 -0700

    Additional Models

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const Customer = require('./models/Customer');
 const Order = require('./models/Order');
+const Error = require('./models/Error');
+const LoginAttempt = require('./models/LoginAttempt');
+const User = require('./models/User');
 module.exports = {
   ...Customer,
-  ...Order
+  ...Order,
+  ...Error,
+  ...LoginAttempt,
+  ...User
 };
+console.log(Error);
